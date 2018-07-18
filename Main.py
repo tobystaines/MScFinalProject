92b1bf8d570f16d777231843c12f28e247348179
Toby Staines <tobystaines@gmail.com>
2018-07-18 11:16:35 +0100

    Completed basic training
    also began adding summaries

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ def cfg():
     model_config = {"model_base_dir": "checkpoints",  # Base folder for model checkpoints
                     "log_dir": "logs",  # Base folder for log files
                     "data_root": 'C:/Users/Toby/Jupyter Notebooks/My Work/MSc Project/Test Audio/GANdatasetsMini/',  # Base folder of CHiME 3 dataset
-                    'SAMPLE_RATE': 44100,  # Desired sample rate of audio. Inout will be resampled to this
+                    'SAMPLE_RATE': 44100,  # Desired sample rate of audio. Input will be resampled to this
                     'N_FFT': 1024,  # Number of samples in each fourier transform
                     'FFT_HOP': 256,  # Number of samples between the start of each fourier transform
                     'N_CHANNELS' : 1,  # May be removed - all data is single channel
@@ -26,34 +26,84 @@ def cfg():
                     'PATCH_HOP': 128,
                     'BATCH_SIZE': 8,
                     'N_SHUFFLE': 20,
-                    'EPOCHS': 1
+                    'EPOCHS': 1,  # Number of full passes through the dataset to train for
+                    'EARLY_STOPPING': True,  # Should validation data checks be used for early stopping?
+                    'VAL_ITERS': 15,  # Number of training iterations between validation checks,
+                    'NUM_WORSE_VAL_CHECKS': 2  # Number of successively worse validation checks before early stopping
                     }
 
     experiment_id = np.random.randint(0,1000000)
 
 
 @ex.capture
-def test(sess, data, model, model_config):
-    #_, cost = sess.run([model.train_op, model.cost], {model.is_training: False})
-    pass
+def train(sess, model, model_config, handle, training_iterator, training_handle, validation_iterator, validation_handle, writer):
 
+    #  Begin training loop
+    print('Starting training')
+    epoch = 1
+    iteration = 1
+    min_val_cost = 1
+    worse_val_checks = 0
+    sess.run(training_iterator.initializer)
+    # Train for the specified number of epochs, unless early stopping is triggered
+    while epoch < model_config['EPOCHS'] + 1 and worse_val_checks < model_config['NUM_WORSE_VAL_CHECKS']:
+        try:
+            _, cost = sess.run([model.train_op, model.cost], {model.is_training: True, handle: training_handle})
+            if iteration % 10 == 0:
+                print("       Training iteration: {i}, Loss: {l}".format(i=iteration, l=cost))
+            writer.add_summary(cost, iteration)
+            # If using early stopping, enter validation loop
+            if model_config['EARLY_STOPPING'] and iteration % model_config['VAL_ITERS'] == 0:
+                print('Validating')
+                sess.run(validation_iterator.initializer)
+                val_costs = list()
+                while True:
+                    try:
+                        val_cost = sess.run(model.cost, {model.is_training: False, handle: validation_handle})
+                        val_costs.append(val_cost)
+                    except tf.errors.OutOfRangeError:
+                        val_check_mean_cost = sum(val_costs) / len(val_costs)
+                        print('Validation check mean loss: {l}'.format(l=val_check_mean_cost))
+                        if val_check_mean_cost > min_val_cost:  # If validation loss has worsened, take note
+                            worse_val_checks += 1
+                            print('Validation loss has worsened. worse_val_checks = {w}'.format(w=worse_val_checks))
+                        else:  # If validation cost has improved, reset counter
+                            min_val_cost = val_check_mean_cost
+                            worse_val_checks = 0
+                            print('Validation loss has improved!')
+                        break
+            iteration += 1
+        # When the dataset is exhausted, note the end of the epoch
+        except tf.errors.OutOfRangeError:
+            print('Epoch {e} finished.'.format(e=epoch))
+            epoch += 1
+            sess.run(training_iterator.initializer)
+    print('Finished requested number of epochs. Training complete.')
 
+    return model
 
 
 @ex.capture
-def train(sess, data, model_config):
-
-    # Build model
-
-
-    # Summaries
-    # Start session
-    # Load checkpoint model if required
-
-
-    # Training finished - save model and close session
-
-    pass
+def test(sess, model, handle, testing_iterator, testing_handle):
+    # Testing - move to test func later
+    # Calculate L1 loss
+    print('Starting testing')
+    sess.run(testing_iterator.initializer)
+    iteration = 1
+    test_costs = list()
+    while True:
+        try:
+            cost = sess.run(model.cost, {model.is_training: False, handle: testing_handle})
+            if iteration % 10 == 0:
+                print("       Testing iteration: {i}, Loss: {l}".format(i=iteration, l=cost))
+            test_costs.append(cost)
+            iteration += 1
+        except tf.errors.OutOfRangeError:
+            mean_cost = sum(test_costs) / len(test_costs)
+            print('Testing complete. Mean loss over test set: {l}'.format(l=mean_cost))
+            break
+            # Calculate audio loss metrics
+    return mean_cost
 
 
 @ex.capture
@@ -62,81 +112,51 @@ def optimise():
 
 
 @ex.automain
-def do_experiment(model_config):
+def do_experiment(model_config, experiment_id):
 
-    #tf.reset_default_graph()
     # Prepare data
     print('Preparing dataset')
     train_data, val_data, test_data = Dataset.prepare_datasets(model_config)
 
-    #  Start session
+    # Start session
     sess = tf.Session()
 
-    #  Start training
-    #model = train(sess, train_data, model_config)
-
-    #  Create iterators
-    iterator = train_data.make_initializable_iterator()
+    # Create iterators
+    handle = tf.placeholder(tf.string, shape=[])
+    iterator = tf.data.Iterator.from_string_handle(handle, train_data.output_types, train_data.output_shapes)
     mixed, voice = iterator.get_next()
 
-    training_init_op = iterator.make_initializer(train_data)
-    validation_init_op = iterator.make_initializer(val_data)
-    testing_init_op = iterator.make_initializer(test_data)
+    training_iterator = train_data.make_initializable_iterator()
+    validation_iterator = val_data.make_initializable_iterator()
+    testing_iterator = test_data.make_initializable_iterator()
 
-    #  Create variable placeholders
+    training_handle = sess.run(training_iterator.string_handle())
+    validation_handle = sess.run(validation_iterator.string_handle())
+    testing_handle = sess.run(testing_iterator.string_handle())
+
+    # Create variable placeholders
     is_training = tf.placeholder(shape=(), dtype=bool)
     mixed_mag = mixed[0][:, :, 1:, :2]  # Yet more hacking to get around this tuple problem
     mixed_phase = mixed[0][:, :, 1:, 2:]
     voice_mag = voice[0][:, :, 1:, :2]
 
-    #  Build U-Net model
+    # Build U-Net model
     print('Creating model')
-    model = UNet.UNetModel(
-        mixed_mag,
-        voice_mag,
-        mixed_phase,
-        is_training
-    )
+    model = UNet.UNetModel(mixed_mag, voice_mag, mixed_phase, is_training)
+
+    # Summaries
+    model_folder = str(experiment_id)
+    tf.summary.scalar('L1_loss', model.cost)
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter(os.path.join(model_config["log_dir"], model_folder), graph=sess.graph)
+
+    # Train the model
     sess.run(tf.global_variables_initializer())
+    model = train(sess, model, model_config, handle, training_iterator, training_handle, validation_iterator, validation_handle, writer)
 
-    #  Begin training loop
-    print('Starting training')
-    epoch = 1
-    iteration = 1
-    sess.run(training_init_op)
-    while epoch < model_config['EPOCHS'] + 1:
-
-        try:
-            _, cost = sess.run([model.train_op, model.cost], {model.is_training: True})
-            if iteration % 10 == 0:
-                print("            , {0}, {1}".format(iteration, cost))
-            iteration += 1
-        except tf.errors.OutOfRangeError:
-            print('Epoch {e} finished.'.format(e=epoch))
-            epoch += 1
-            sess.run(training_init_op)
-    print('Finished requested number of epochs. Training complete.')
-
-    #  Test trained model
-    # Testing - move to test func later
-    # Calculate L1 loss
-    print('Starting testing')
-    sess.run(testing_init_op)
-    iteration = 1
-    test_costs = list()
-    while True:
-        try:
-            cost = sess.run(model.cost, {model.is_training: False})
-            if iteration % 10 == 0:
-                print("            , {0}, {1}".format(iteration, cost))
-            test_costs.append(cost)
-            iteration += 1
-        except tf.errors.OutOfRangeError:
-            mean_cost = sum(test_costs)/len(test_costs)
-            print('Testing complete. Mean cost over test set: {c}'.format(c=mean_cost))
-            break
-    # Calculate audio loss metrics
+    # Test trained model
+    mean_test_loss = test(sess, model, handle, testing_iterator, testing_handle)
+    print(mean_test_loss, '\nAll done!')
 
 
 
-    pass
