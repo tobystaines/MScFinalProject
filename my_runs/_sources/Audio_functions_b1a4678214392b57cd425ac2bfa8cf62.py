b52741425ffb9832358be39d136f3a04850b87c3
tobystaines <tobystaines@gmail.com>
2018-08-09 12:19:57 +0100

    New dataset construction first test - failed

---
+++ my_runs/_sources/Audio_functions_b1a4678214392b57cd425ac2bfa8cf62.py
@@ -0,0 +1,195 @@
+import tensorflow as tf
+import numpy as np
+import librosa
+
+
+def read_audio(path, sample_rate, n_channels=1):
+
+    def read_audio_py(py_path):
+        # if n_channels == 1:
+        mono, _ = librosa.load(py_path, sr=sample_rate, mono=True)
+        return np.expand_dims(mono, 1)
+        # elif n_channels == 2:
+        # stereo, _ = librosa.load(py_path, sr=sample_rate, mono=False)
+        # return stereo.T
+        # else:
+        # raise ValueError('Invalid channels: %d' % n_channels)
+
+    return tf.py_func(read_audio_py, [path], tf.float32, stateful=False)
+
+
+def read_audio_pair(path_a, path_b, sample_rate):
+    """
+    Takes in the path of two audio files and the required output sample rate,
+    returns a tuple of tensors of the wave form of the audio files.
+    """
+    def read_audio_py(py_path):
+
+        mono, _ = librosa.load(py_path, sr=sample_rate, mono=True)
+        return np.expand_dims(mono, 1)
+
+    return (tf.py_func(read_audio_py, [path_a], tf.float32, stateful=False),
+            tf.py_func(read_audio_py, [path_b], tf.float32, stateful=False))
+
+
+def fake_stereo(audio):
+
+    def fake_stereo(x):
+        return tf.stack([x, x], 1)
+
+    voice = audio[:, 0]
+    mixed = voice * 2
+    return fake_stereo(mixed), fake_stereo(voice)
+
+
+def compute_spectrogram(audio, n_fft, fft_hop, n_channels=1, normalise=False):
+    '''
+    Parameters
+    ----------
+    audio : single to dual channel audio shaped (n_samples, n_channels)
+
+    Returns
+    -------
+    Tensor of shape (n_frames, 1 + n_fft / 2, n_channels * 2), where the
+        last dimension is (left_mag, right_mag, left_phase, right_phase)
+    '''
+
+    def stft(x, normalise):
+        spec = librosa.stft(
+            x, n_fft=n_fft, hop_length=fft_hop, window='hann')
+        mag = np.abs(spec)
+        if normalise:
+            # TODO: normalize?
+            mag = (mag-mag.min())/(mag.max()-mag.min())
+        return mag, np.angle(spec)
+
+    # def stereo_func(py_audio):
+    #    left_mag, left_phase = stft(py_audio[:, 0])
+    #    right_mag, right_phase = stft(py_audio[:, 1])
+    #    ret = np.array([left_mag, right_mag, left_phase, right_phase]).T
+    #    return ret.astype(np.float32)
+
+    def mono_func(py_audio, normalise):
+        mag, phase = stft(py_audio[:, 0], normalise)
+        ret = np.array([mag, phase]).T
+        return ret.astype(np.float32)
+
+    # if n_channels == 2:
+    #    func = stereo_func
+    # elif n_channels == 1:
+    #    func = mono_func
+    # else:
+    #    raise ValueError('Invalid channels: %d' % n_channels)
+
+    with tf.name_scope('read_spectrogram'):
+        ret = tf.py_func(mono_func, [audio, normalise], tf.float32, stateful=False)
+        ret.set_shape([None, 1 + n_fft / 2, 2])   # n_channels * 2])
+    return ret
+
+
+def extract_spectrogram_patches(
+        spec, n_fft, n_channels, patch_window, patch_hop):
+    '''
+    Parameters
+    ----------
+    spec : Spectrogram of shape (n_frames, 1 + n_fft / 2, n_channels * 2)
+
+    Returns
+    -------
+    Tensor of shape (n_patches, patch_window, 1 + n_fft / 2, n_channels * 2)
+        containing patches from spec.
+    '''
+    with tf.name_scope('extract_spectrogram_patches'):
+        spec4d = tf.expand_dims(spec, 0)
+
+        patches = tf.extract_image_patches(
+            spec4d, ksizes=[1, patch_window, 1 + n_fft / 2, 1],
+            strides=[1, patch_hop, 1 + n_fft / 2, 1],
+            rates=[1, 1, 1, 1],
+            padding='VALID'
+        )
+
+        num_patches = tf.shape(patches)[1]
+
+        return tf.reshape(patches, [num_patches, patch_window,
+                                    int(1 + n_fft / 2), 2])
+
+
+def replace_spectrogram_patches(patches):
+    '''
+    Parameters
+    ----------
+    Tensor of shape (n_patches, patch_window, 1 + n_fft / 2, n_channels * 2)
+    containing patches from spec.
+
+    Returns
+    -------
+    spec : Spectrogram of shape (n_frames, 1 + n_fft / 2, n_channels * 2)
+    '''
+    # Need to account for patch overlap
+    # Is it possible to account for no set num patches per whole spectrogram?
+    pass
+
+
+def hwr_tf(x):
+    return x * tf.cast(x > 0.0, tf.float32)
+
+
+def compute_acapella_diff(mixed, noise):
+    mixed_mag = mixed[:, :, 0:, :2]
+    mixed_phase = mixed[:, :, 0:, 2:]
+    noise_mag = noise[:, :, 0:, :2]
+    voice_mag = hwr_tf(mixed_mag - noise_mag) # TODO: normalize?
+    voice_phase = mixed_phase
+    return mixed, noise, tf.concat((voice_mag, voice_phase), axis=3)
+
+
+def spectrogramToAudioFile(magnitude, fftWindowSize, hopSize, phaseIterations=0, phase=None, length=None):
+    '''
+    Computes an audio signal from the given magnitude spectrogram, and optionally an initial phase.
+    Griffin-Lim is executed to recover/refine the given the phase from the magnitude spectrogram.
+    :param magnitude: Magnitudes to be converted to audio
+    :param fftWindowSize: Size of FFT window used to create magnitudes
+    :param hopSize: Hop size in frames used to create magnitudes
+    :param phaseIterations: Number of Griffin-Lim iterations to recover phase
+    :param phase: If given, starts ISTFT with this particular phase matrix
+    :param length: If given, audio signal is clipped/padded to this number of frames
+    :return:
+    '''
+    if phase is not None:
+        if phaseIterations > 0:
+            # Refine audio given initial phase with a number of iterations
+            return reconPhase(magnitude, fftWindowSize, hopSize, phaseIterations, phase, length)
+        # reconstructing the new complex matrix
+        stftMatrix = magnitude * np.exp(phase * 1j) # magnitude * e^(j*phase)
+        audio = librosa.istft(stftMatrix, hop_length=hopSize, length=length)
+    else:
+        audio = reconPhase(magnitude, fftWindowSize, hopSize, phaseIterations)
+    return audio
+
+def reconPhase(magnitude, fftWindowSize, hopSize, phaseIterations=0, initPhase=None, length=None):
+    '''
+    Griffin-Lim algorithm for reconstructing the phase for a given magnitude spectrogram, optionally with a given
+    intial phase.
+    :param magnitude: Magnitudes to be converted to audio
+    :param fftWindowSize: Size of FFT window used to create magnitudes
+    :param hopSize: Hop size in frames used to create magnitudes
+    :param phaseIterations: Number of Griffin-Lim iterations to recover phase
+    :param initPhase: If given, starts reconstruction with this particular phase matrix
+    :param length: If given, audio signal is clipped/padded to this number of frames
+    :return:
+    '''
+    for i in range(phaseIterations):
+        if i == 0:
+            if initPhase is None:
+                reconstruction = np.random.random_sample(magnitude.shape) + 1j * (2 * np.pi * np.random.random_sample(magnitude.shape) - np.pi)
+            else:
+                reconstruction = np.exp(initPhase * 1j) # e^(j*phase), so that angle => phase
+        else:
+            reconstruction = librosa.stft(audio, fftWindowSize, hopSize)
+        spectrum = magnitude * np.exp(1j * np.angle(reconstruction))
+        if i == phaseIterations - 1:
+            audio = librosa.istft(spectrum, hopSize, length=length)
+        else:
+            audio = librosa.istft(spectrum, hopSize)
+    return audio
---
+++ my_runs/_sources/UNet_4f448d726c81096d0f2c58436c980860.py
@@ -0,0 +1,158 @@
+import tensorflow as tf
+import Model_functions as mf
+
+
+class UNetModel(object):
+    """
+    Top level U-Net object.
+    Attributes:
+        mixed: Input placeholder for magnitude spectrogram of mixed signals (voice plus background noise) - X
+        voice: Input placeholder for magnitude spectrogram of isolated voice signal - Y
+        mixed_phase: Input placeholder for phase spectrogram of mixed signals (voice plus background noise)
+        variant: The type of U-Net model (Normal convolutional or capsule based)
+        is_training: Boolean - should the model be trained on the current input or not
+        name: Model instance name
+    """
+    def __init__(self, mixed, voice, mixed_phase, variant, is_training, name):
+        with tf.variable_scope(name):
+            self.mixed = mixed
+            self.voice = voice
+            self.mixed_phase = mixed_phase
+            self.variant = variant
+            self.is_training = is_training
+
+            self.voice_mask_unet = UNet(mixed, variant, is_training=is_training, reuse=False, name='voice-mask-unet')
+
+            self.voice_mask = self.voice_mask_unet.output
+
+            self.gen_voice = self.voice_mask * mixed
+
+            self.cost = mf.l1_loss(self.gen_voice, voice)
+
+            self.optimizer = tf.train.AdamOptimizer(
+                learning_rate=0.0002,
+                beta1=0.5,
+            )
+            self.train_op = self.optimizer.minimize(self.cost)
+
+
+class UNet(object):
+
+    def __init__(self, input_tensor, variant, is_training, reuse, name):
+        with tf.variable_scope(name, reuse=reuse):
+            self.variant = variant
+
+            if self.variant == 'unet':
+                self.encoder = UNetEncoder(input_tensor, is_training, reuse)
+                self.decoder = UNetDecoder(self.encoder.output, self.encoder, is_training, reuse)
+            elif self.variant == 'capsunet':
+                self.encoder = CapsUNetEncoder(input_tensor, is_training, reuse)
+                self.decoder = CapsUNetDecoder(self.encoder.output, self.encoder, is_training, reuse)
+
+            self.output = mf.tanh(self.decoder.output) / 2 + .5
+
+
+class UNetEncoder(object):
+    """
+    The down-convolution side of a convoltional U-Net model.
+    """
+
+    def __init__(self, input_tensor, is_training, reuse):
+        net = input_tensor
+        with tf.variable_scope('encoder'):
+            with tf.variable_scope('layer-1'):
+                net = mf.conv(net, filters=16, kernel_size=5, stride=(2, 2))
+                self.l1 = net
+
+            with tf.variable_scope('layer-2'):
+                net = mf.lrelu(net)
+                net = mf.conv(net, filters=32, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                self.l2 = net
+
+            with tf.variable_scope('layer-3'):
+                net = mf.lrelu(net)
+                net = mf.conv(net, filters=64, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                self.l3 = net
+
+            with tf.variable_scope('layer-4'):
+                net = mf.lrelu(net)
+                net = mf.conv(net, filters=128, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                self.l4 = net
+
+            with tf.variable_scope('layer-5'):
+                net = mf.lrelu(net)
+                net = mf.conv(net, filters=256, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                self.l5 = net
+
+            with tf.variable_scope('layer-6'):
+                net = mf.lrelu(net)
+                net = mf.conv(net, filters=512, kernel_size=5, stride=(2, 2))
+
+            self.output = net
+
+
+class UNetDecoder(object):
+    """
+    The up-convolution side of a convolutional U-Net model
+    """
+    def __init__(self, input_tensor, encoder, is_training, reuse):
+        net = input_tensor
+
+        with tf.variable_scope('decoder'):
+            with tf.variable_scope('layer-1'):
+                net = mf.relu(net)
+                net = mf.deconv(net, filters=256, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                net = mf.dropout(net, .5)
+
+            with tf.variable_scope('layer-2'):
+                net = mf.relu(mf.concat(net, encoder.l5))
+                net = mf.deconv(net, filters=128, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                net = mf.dropout(net, .5)
+
+            with tf.variable_scope('layer-3'):
+                net = mf.relu(mf.concat(net, encoder.l4))
+                net = mf.deconv(net, filters=64, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+                net = mf.dropout(net, .5)
+
+            with tf.variable_scope('layer-4'):
+                net = mf.relu(mf.concat(net, encoder.l3))
+                net = mf.deconv(net, filters=32, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+
+            with tf.variable_scope('layer-5'):
+                net = mf.relu(mf.concat(net, encoder.l2))
+                net = mf.deconv(net, filters=16, kernel_size=5, stride=(2, 2))
+                net = mf.batch_norm(net, is_training=is_training, reuse=reuse)
+
+            with tf.variable_scope('layer-6'):
+                net = mf.relu(mf.concat(net, encoder.l1))
+                net = mf.deconv(net, filters=1, kernel_size=5, stride=(2, 2))
+
+            self.output = net
+
+
+class CapsUNetEncoder(object):
+    """
+    The down-convolutional side of a capsule based U-Net model.
+    """
+    def __init__(self, input_tensor, is_training, reuse):
+        net = input_tensor
+
+        self.output = net
+
+
+class CapsUNetDecoder(object):
+    """
+    The up-convolutional side of a capsule based U-Net model.
+    """
+    def __init__(self, input_tensor, encoder, is_training, reuse):
+        net = input_tensor
+
+        self.output = net
