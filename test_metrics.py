1881f3a792f482cf4efe8650a6b08a3a66c99888
Toby Staines <tobystaines@gmail.com>
2018-09-14 16:07:11 +0100

    test_metrics commentry

--- test_metrics.py
+++ test_metrics.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import csv
 import pickle
 import datetime
@@ -7,6 +8,13 @@ import numpy as np
 import mir_eval
 import Audio_functions as af
 
+"""
+This script takes the results of a test set being passed through a model, converts the relevant parts from spectrogram 
+to audio and then calculates audio quality metrics.
+
+This process causes a bottleneck and is not on the critical path towards training a model. As such, required data has 
+been dumped to pickle files by the main script, so that this script can be run on a different server.
+"""
 
 def get_test_metrics(experiment_id):
 
@@ -15,6 +23,7 @@ def get_test_metrics(experiment_id):
     file_list = glob(dump_folder + '/*')
     test_num = max([int(file.split('_')[2]) for file in file_list]) + 1
     metrics = list()
+    #  For each test run, calculate the results
     for test in range(test_num):
         print('{ts}:\tProcessing test {t}'.format(ts=datetime.datetime.now(), t=test))
         test_files = [file for file in file_list if file.split('_')[2] == str(test)]
@@ -24,6 +33,7 @@ def get_test_metrics(experiment_id):
         sars = list()
         nsdrs = list()
 
+        #  There will be one pickle file per batch. For each one, load it and calculate the metrics
         for file in test_files:
             cost, voice_est_mag, voice, mixed_audio, mixed_phase, model_config = pickle.load(open(file, 'rb'))
             test_costs.append(cost)
@@ -45,6 +55,7 @@ def get_test_metrics(experiment_id):
                 sars.append(sar[0])
                 nsdrs.append(nsdr)
 
+        #  Record mean results for each metric across all batches in the test
         mean_cost = sum(test_costs) / len(test_costs)
         mean_sdr = sum(sdrs) / len(sdrs)
         mean_sir = sum(sirs) / len(sirs)
@@ -53,6 +64,10 @@ def get_test_metrics(experiment_id):
         metrics.append({'test': test, 'mean_cost': mean_cost, 'mean_sdr': mean_sdr, 'mean_sir': mean_sir,
                         'mean_sar': mean_sar, 'mean_nsdr': mean_nsdr})
 
+    #  Write the results from the experiment to a CSV file, one row per test
+
+    if not os.path.isdir('test_metrics'):
+        os.mkdir('test_metrics')
     file_name = 'test_metrics/' + experiment_id + '.csv'
     with open(file_name, 'w') as csvfile:
         fieldnames = ['test', 'mean_cost', 'mean_sdr', 'mean_sir', 'mean_sar', 'mean_nsdr']
