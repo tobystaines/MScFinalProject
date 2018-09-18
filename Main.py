48db81b36b047796be9af200fe23981f434946cb
Toby Staines <tobystaines@gmail.com>
2018-09-18 15:11:54 +0100

    model 26 retest

--- Main.py
+++ Main.py
@@ -19,20 +19,20 @@ ex.observers.append(FileStorageObserver.create('my_runs'))
 @ex.config
 def cfg():
     model_config = {'saving': True,  # Whether to take checkpoints
-                    'loading': False,  # Whether to load an existing checkpoint
-                    'dataset': 'LibriSpeech',  # Choice of 'LibriSpeech', 'CHiME', or 'both'
+                    'loading': True,  # Whether to load an existing checkpoint
+                    'dataset': 'CHiME',  # Choice of 'LibriSpeech', 'CHiME', or 'both'
                     'local_run': False,  # Whether experiment is running on laptop or server
-                    'checkpoint_to_load': "43/43-1001",  # Checkpoint format: run/run-epoch
-                    'INITIALISATION_TEST': True,  # Whether or not to calculate test metrics before training
+                    'checkpoint_to_load': "26/26-21",  # Checkpoint format: run/run-epoch
+                    'INITIALISATION_TEST': False,  # Whether or not to calculate test metrics before training
                     'SAMPLE_RATE': 16384,  # Desired sample rate of audio. Input will be resampled to this
                     'N_FFT': 1024,  # Number of samples in each fourier transform
                     'FFT_HOP': 256,  # Number of samples between the start of each fourier transform
                     'N_PARALLEL_READERS': 16,
                     'PATCH_WINDOW': 256,  # Number of fourier transforms (rows) in each patch
                     'PATCH_HOP': 128,  # Number of fourier transforms between the start of each patch
-                    'BATCH_SIZE': 5,  # Number of patches in each batch
-                    'N_SHUFFLE': 10,  # Number of patches buffered before batching
-                    'EPOCHS': 500,  # Number of full passes through the dataset to train for
+                    'BATCH_SIZE': 50,  # Number of patches in each batch
+                    'N_SHUFFLE': 1000,  # Number of patches buffered before batching
+                    'EPOCHS': 0,  # Number of full passes through the dataset to train for
                     'EARLY_STOPPING': False,  # Should validation data checks be used for early stopping?
                     'VAL_BY_EPOCHS': True,  # Validation at end of each epoch or every 'val_iters'?
                     'VAL_ITERS': 2000,  # Number of training iterations between validation checks,
