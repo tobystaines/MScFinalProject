3ee0ada8f360e80cd5f4e49fc0c265fa749bb6fd
Toby Staines <tobystaines@gmail.com>
2018-09-21 08:40:20 +0100

    remove utils file

--- Audio_functions.py
+++ Audio_functions.py
@@ -199,3 +199,38 @@ def reconPhase(magnitude, fftWindowSize, hopSize, phaseIterations=0, initPhase=N
         else:
             audio = librosa.istft(spectrum, hopSize)
     return audio
+
+
+def zip_tensor_slices(*args):
+    '''
+    Parameters
+    ----------
+    *args : list of _n_ _k_-dimensional tensors, where _k_ >= 2
+        The first dimension has _m_ elements.
+
+    Returns
+    -------
+    result : Dataset of _m_ examples, where each example has _n_
+        records of _k_ - 1 dimensions.
+
+    Example
+    -------
+    ds = (
+        tf.data.Dataset.zip((
+            tf.data.Dataset.from_tensors([[1,2], [3,4], [5, 6]]),
+            tf.data.Dataset.from_tensors([[10, 20], [30, 40], [50, 60]])
+        ))
+        .flat_map(zip_tensor_slices)  # <--- *HERE*
+    )
+    el = ds.make_one_shot_iterator().get_next()
+    print sess.run(el)
+    print sess.run(el)
+
+    # Output:
+    # (array([1, 2], dtype=int32), array([10, 20], dtype=int32))
+    # (array([3, 4], dtype=int32), array([30, 40], dtype=int32))
+    '''
+    return tf.data.Dataset.zip(tuple([
+        tf.data.Dataset.from_tensor_slices(arg)
+        for arg in args
+    ]))
