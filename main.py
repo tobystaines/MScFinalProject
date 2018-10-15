ae3371a34c73b7b4542404b82effe18905d26fed
Toby Staines <tobystaines@gmail.com>
2018-10-15 19:42:13 +0100

    Prep for training

--- main.py
+++ main.py
@@ -20,30 +20,30 @@ ex.observers.append(FileStorageObserver.create('my_runs'))
 def cfg():
     model_config = {'model_variant': 'basic_capsnet',  # The type of model to use, from ['unet', capsunet', basic_capsnet']
                     'mag_phase': True,  # Whether to use a magnitude/phase or complex number representation of the spectrogram
-                    'initialisation_test': False,  # Whether or not to calculate test metrics before training
-                    'loading': True,  # Whether to load an existing checkpoint
+                    'initialisation_test': True,  # Whether or not to calculate test metrics before training
+                    'loading': False,  # Whether to load an existing checkpoint
                     'checkpoint_to_load': "115/115-473",  # Checkpoint format: run/run-step
                     'saving': True,  # Whether to take checkpoints
-                    'save_by_epochs': False,  # Checkpoints at end of each epoch or every 'save_iters'?
+                    'save_by_epochs': True,  # Checkpoints at end of each epoch or every 'save_iters'?
                     'save_iters': 1000,  # Number of training iterations between checkpoints
                     'early_stopping': True,  # Should validation data checks be used for early stopping?
                     'val_by_epochs': True,  # Validation at end of each epoch or every 'val_iters'?
                     'val_iters': 50000,  # Number of training iterations between validation checks,
                     'num_worse_val_checks': 3,  # Number of successively worse validation checks before early stopping,
-                    'dataset': 'CHiME and LibriSpeech_m',  # Choice from ['CHiME', 'LibriSpeech_s', 'LibriSpeech_m',
+                    'dataset': 'CHiME and LibriSpeech_s',  # Choice from ['CHiME', 'LibriSpeech_s', 'LibriSpeech_m',
                                         #               'LibriSpeech_l', 'CHiME and LibriSpeech_s',
                                         #               'CHiME and LibriSpeech_m', 'CHiME and LibriSpeech_l']
                     'local_run': False,  # Whether experiment is running on laptop or server
-                    'sample_rate': 8192,  # Desired sample rate of audio. Input will be resampled to this
+                    'sample_rate': 16384,  # Desired sample rate of audio. Input will be resampled to this
                     'n_fft': 1024,  # Number of samples in each fourier transform
                     'fft_hop': 256,  # Number of samples between the start of each fourier transform
                     'n_parallel_readers': 16,
-                    'patch_window': 128,  # Number of fourier transforms (rows) in each patch
-                    'patch_hop': 64,  # Number of fourier transforms between the start of each patch
+                    'patch_window': 256,  # Number of fourier transforms (rows) in each patch
+                    'patch_hop': 128,  # Number of fourier transforms between the start of each patch
                     'batch_size': 5,  # Number of patches in each batch
                     'n_shuffle': 1000,  # Number of patches buffered before batching
                     'learning_rate': 0.0002,  # The learning rate to be used by the model
-                    'epochs': 5,  # Number of full passes through the dataset to train for
+                    'epochs': 7,  # Number of full passes through the dataset to train for
                     'normalise_mag': True,  # Are magnitude spectrograms normalised in pre-processing?
                     'GPU': '0'
                     }
